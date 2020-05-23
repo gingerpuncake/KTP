7d13e99ce9b6edfd27a3bdf86b2e51a0edacb594
gingerpuncake <64023549+gingerpuncake@users.noreply.github.com>
2020-05-23 15:05:18 +0300

    'Lab7Modificatio'

--- 7 лабораторная/src/Crawler.java
+++ 7 лабораторная/src/Crawler.java
@@ -1,64 +1,231 @@
 import java.io.*;
 import java.net.*;
-import java.util.LinkedList;
-
+import java.util.*;
+import java.util.regex.*;
 public class Crawler {
 
-    static LinkedList <URLDepthPair> findLink = new LinkedList <URLDepthPair>();
-    static LinkedList <URLDepthPair> viewedLink = new LinkedList <URLDepthPair>();
+    public static void main(String[] args) throws IOException {
 
+        String[] arg = new String[] {"https://htmlbook.ru//","1"};
+        // глубина
+        int depth = 0;
+        // проверяем, сколько параментров введено
+        if (arg.length != 2) {
+            // если не 2, выводится ошибка, работа программы останавливается
+            System.out.println("usage: java Crawler <URL> <depth>");
+            System.exit(1);
+        }
+        // если все введено верно, то
+        else {
+            try {
+                // устанавливаем глубину
+                depth = Integer.parseInt(arg[1]);
+            }
+            // проверяем является ли глубина цифрой
+            catch (NumberFormatException nfe) {
+                System.out.println("usage: java Crawler <URL> <depth>");
+                System.exit(1);
+            }
+        }
+        // список для представления ожидающих URL-адресов
+        LinkedList<URLDepthPair> pendingURLs = new LinkedList<URLDepthPair>();
+        //  связный список для представления обработанных URL
+        LinkedList<URLDepthPair> processedURLs = new LinkedList<URLDepthPair>();
+        // глубина URL-пары для представления веб-сайта
+        URLDepthPair currentDepthPair = new URLDepthPair(arg[0], 0);
+        // добавляем введенный веб-сайт в ожидающие URL-адреса
+        pendingURLs.add(currentDepthPair);
+        // добавляем текущий веб-сайт
+        ArrayList<String> seenURLs = new ArrayList<String>();
+        seenURLs.add(currentDepthPair.getURL());
 
-    public static void showResult(LinkedList<URLDepthPair> viewedLink) {
-        for (URLDepthPair c : viewedLink)
-            System.out.println("Depth: "+c.getDepth() + "  Link: "+c.getURL());
-    }
-
-
-    public static void request(PrintWriter out,URLDepthPair pair) throws MalformedURLException {
-        out.println("GET " + pair.getPath() + " HTTP/1.1");
-        out.println("Host: " + pair.getHost());
-        out.println("Connection: close");
-        out.println();
-        out.flush();
-    }
-
-    public static void Process(String pair, int maxDepth) throws IOException {
-        findLink.add(new URLDepthPair(pair, 0));
-        while (!findLink.isEmpty()) {
-            URLDepthPair currentPair = findLink.removeFirst();
-            if (currentPair.depth<maxDepth){
-                Socket my_socket = new Socket(currentPair.getHost(), 80);
-                my_socket.setSoTimeout(100000);
-                BufferedReader in = new BufferedReader(new InputStreamReader(my_socket.getInputStream()));
-                PrintWriter out = new PrintWriter(my_socket.getOutputStream(), true);
-                request(out, currentPair);
-                String line;
-                while ((line = in.readLine()) != null) {
-                    if (line.indexOf(currentPair.URL_PREFIX)!=-1) {
-                        StringBuilder currentLink = new StringBuilder();
-                        for (int i = line.indexOf(currentPair.URL_PREFIX) + 9; line.charAt(i) != '"'; i++) {
-                            currentLink.append(line.charAt(i));
-                        }
-                        URLDepthPair newPair = new URLDepthPair(currentLink.toString(), currentPair.depth + 1);
-                        if (currentPair.check(findLink, newPair) && currentPair.check(viewedLink, newPair) && !currentPair.URL.equals(newPair.URL))
-                            findLink.add(newPair);
-
+        // пока URL-адреса не станут пустыми, получаем все ссылки от каждого посещенного сайта
+        while (pendingURLs.size() != 0) {
+            // добавляем URL в список обработанных и сохранем глубину
+            URLDepthPair depthPair = pendingURLs.pop();
+            processedURLs.add(depthPair);
+            int myDepth = depthPair.getDepth();
+            // получаем все ссылки с сайта и сохраняем их в новом связном списке
+            LinkedList<String> linksList;
+            linksList = Crawler.getAllLinks(depthPair);
+            // добавляем ссылки, которые раньше не были видны, если мы не достигли максимальной глубины
+            if (myDepth < depth) {
+                // перебирать ссылки с сайта
+                for (String newURL : linksList) {
+                    // продолжаем, если мы уже видели ссылку
+                    if (seenURLs.contains(newURL)) {
+                        continue;
+                    }
+                    // cоздаем новую пару URL-глубина, если ссылка не встречалась ранее, и добвляем в списки
+                    else {
+                        URLDepthPair newDepthPair = new URLDepthPair(newURL, myDepth + 1);
+                        pendingURLs.add(newDepthPair);
+                        seenURLs.add(newURL);
                     }
                 }
-                my_socket.close();
             }
-
-            viewedLink.add(currentPair);
         }
-        showResult(viewedLink);
+        // выводим все обработанные URL с глубиной
+        for (URLDepthPair processedURL : processedURLs) {
+            System.out.println(processedURL);
+        }
     }
-    public static void main(String[] args) {
-
-        String[] arg = new String[]{"http://government.ru/","3"};
+    private static LinkedList<String> getAllLinks(URLDepthPair myDepthPair) throws IOException {
+        // создаем связанный список LinkedList<String>, в котором будут храниться ссылки, что мы находим
+        LinkedList<String> URLs = new LinkedList<String>();
+        // Новый сокет
+        Socket sock;
+        // Новый сокет содержащей имя хоста, и из номера порта, равного 80 (http)
         try {
-            Process(arg[0], Integer.parseInt(arg[1]));
-        } catch (NumberFormatException | IOException e) {
-            System.out.println("usage: java Crawler <URL><depth>");
+            sock = new Socket(myDepthPair.getWebHost(), 80);
         }
+        // ловим исключения неизвестного хоста и возвращаем пустой список
+        catch (UnknownHostException e) {
+            System.err.println("UnknownHostException: " + e.getMessage());
+            return URLs;
+        }
+        // ловим исключения ввода-вывода и возвращаем пустой список
+        catch (IOException ex) {
+            return URLs;
+        }
+        try {
+            // устанавливаем таймаут сокета на 3 секунды
+            sock.setSoTimeout(3000);
+        }
+        // ловим исключения сокета и возвращаем пустой список
+        catch (SocketException exc) {
+            System.err.println("SocketException: " + exc.getMessage());
+            return URLs;
+        }
+        // поля для хранения пути к файлу и хоста
+        String docPath = myDepthPair.getDocPath();
+        String webHost = myDepthPair.getWebHost();
+        // создаем поток вывода
+        OutputStream outStream;
+        // возвращаем OutputStream связанный с сокетом используемый для передачи данных
+        try {
+            outStream = sock.getOutputStream();
+        }
+        // ловим исключения ввода-вывода и возвращаем пустой список
+        catch (IOException exce) {
+            //System.err.println("IOException: " + exce.getMessage());
+            return URLs;
+        }
+        // создаем PrintWriter, true означает, что PrintWriter будет сбрасываться после каждого вывода
+        PrintWriter myWriter = new PrintWriter(outStream, true);
+        if (docPath.length() == 0) {
+            // Отправляем запрос на сервер
+            myWriter.println("GET / HTTP/1.1");
+            myWriter.println("Host: " + webHost);
+            myWriter.println("Connection: close");
+            myWriter.println();
+        }
+        else {
+            myWriter.println("GET " + docPath + " HTTP/1.1");
+            myWriter.println("Host: " + webHost);
+            myWriter.println("Connection: close");
+            myWriter.println();
+        }
+        // создаем поток ввода
+        InputStream inStream;
+        // возвращает InputStream связанный с объектом Socket используемый для приема данных
+        try {
+            inStream = sock.getInputStream();
+        }
+        // ловим исключения ввода-вывода и возвращаем пустой список
+        catch (IOException excep) {
+            //System.err.println("IOException: " + excep.getMessage());
+            return URLs;
+        }
+        // Создаем новый InputStreamReader и BufferedReader для чтения строк с сервера
+        InputStreamReader inStreamReader = new InputStreamReader(inStream);
+        BufferedReader BuffReader = new BufferedReader(inStreamReader);
+        // проверям код ответа сервера
+        int serverCode = 0;
+        String lineCode;
+        try {
+            lineCode = BuffReader.readLine();
+        }
+        // ловим исключения ввода-вывода и возвращаем пустой список
+        catch (IOException except) {
+            return URLs;
+        }
+        // создаем паттерн для кодов html: 2xx, 3xx, 4xx
+        Pattern patternCode = Pattern.compile("(2|3|4)[0-9]{2}");
+        Matcher matcherCode = patternCode.matcher(lineCode);
+        // поиск кода html: 2xx, 3xx, 4xx
+        while (matcherCode.find()) {
+            serverCode = Integer.valueOf(lineCode.substring(matcherCode.start(), matcherCode.end() - 2));
+        }
+        // Обработка для кодов html, равных 2xx
+        if (serverCode == 2) {
+            while (true) {
+                String line;
+                try {
+                    line = BuffReader.readLine();
+                }
+                // ловим исключения ввода-вывода и возвращаем пустой список
+                catch (IOException except) {
+                    return URLs;
+                }
+
+                // прекращаем чтения документа
+                if (line == null) {
+                    break;
+                }
+                // создаем паттерн для поиска URL
+                Pattern patternURL = Pattern.compile(
+                        "[\"]"// перед ссылкой должно быть кавычка
+                                + "[https?://]{7,8}" + "([w]{3})?" + "[\\w\\.\\-]+" + "\\." + "[A-Za-z]{2,6}" + "[\\w\\.-/]*"
+                                + "[\"]"); // после ссылки должно быть кавычка
+                Matcher matcherURL = patternURL.matcher(line);
+                // поиск URL в строке с помощью паттерна
+                while (matcherURL.find()) {
+                    String newLink = line.substring(matcherURL.start() + 1,
+                            matcherURL.end() - 1);
+                    // добавляем ссылки в список URLs
+                    URLs.add(newLink);
+                }
+            }
+            sock.close();
+            // возвращаем все ссылки на рассматриваемом сайте.
+            return URLs;
+        }
+        // обработка для кодов html, равных 3xx
+        if (serverCode == 3) {
+            // поле для исправленного URL
+            String newURL = "";
+            String tempLine;
+            while (true) {
+                try {
+                    tempLine = BuffReader.readLine();
+                }
+                // ловим исключения ввода-вывода и возвращаем пустой список
+                catch (IOException except) {
+                    return URLs;
+                }
+                // прекращаем чтения документа
+                if (tempLine == null) {
+                    break;
+                }
+
+                Pattern patternNewURL = Pattern.compile("(Location: ){1}[\\S]+");
+                Matcher matcherNewURL = patternNewURL.matcher(tempLine);
+                while (matcherNewURL.find()) {
+                    newURL = tempLine.substring(matcherNewURL.start() + 10,
+                            matcherNewURL.end());
+                }
+            }
+            if (newURL.equals(myDepthPair.getURL())) {
+                sock.close();
+                // возвращаем пустой список ссылок
+                return URLs;
+            }
+            URLDepthPair newDepthPair;
+            newDepthPair = new URLDepthPair(newURL, myDepthPair.getDepth());
+            // вызываем метод getAllLinks с исправленным URL
+            return getAllLinks(newDepthPair);
+        }
+        return URLs;
     }
 }
--- 7 лабораторная/src/URLDepthPair.java
+++ 7 лабораторная/src/URLDepthPair.java
@@ -1,39 +1,51 @@
-import java.util.LinkedList;
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import java.net.*;
 public class URLDepthPair {
-    public static final String URL_PREFIX = "<a href=\"http";
 
-    public String URL;
-    public int depth;
+    private final String currentURL;
+    private final int currentDepth;
 
-    public URLDepthPair (String URL, int depth){
-        this.URL=URL;
-        this.depth=depth;
+    // конструктор
+    public URLDepthPair(String URL, int depth) {
+        // текущий URL
+        currentURL = URL;
+        // глубина
+        currentDepth = depth;
     }
 
-
-    public String getHost() throws MalformedURLException {
-        URL host = new URL(URL);
-        return host.getHost();
-    }
-    public String getPath() throws MalformedURLException {
-        URL path = new URL(URL);
-        return path.getPath();
-    }
-    public int getDepth() {
-        return depth;
-    }
+    // метод, который возвращает текущий URL
     public String getURL() {
-        return URL;
+        return currentURL;
+    }
+    // метод, который возвращает текущую глубину
+    public int getDepth() {
+        return currentDepth;
     }
 
-    public static boolean check(LinkedList<URLDepthPair> resultLink, URLDepthPair pair) {
-        boolean isAlready = true;
-        for (URLDepthPair c : resultLink)
-            if (c.toString().equals(pair.toString()))
-                isAlready=false;
-        return isAlready;
+    // текущий URL и глубина, но в виде строки
+    public String toString() {
+        String stringDepth = Integer.toString(currentDepth);
+        return stringDepth + '\t' + currentURL;
+    }
+
+    // метод, который возвращает путь документа текущего URL
+    public String getDocPath() {
+        try {
+            URL url = new URL(currentURL);
+            return url.getPath();
+        }
+        catch (MalformedURLException e) {
+            return null;
+        }
+    }
+
+    // метод, который возращает хост текущего URL
+    public String getWebHost() {
+        try {
+            URL url = new URL(currentURL);
+            return url.getHost();
+        }
+        catch (MalformedURLException e) {
+            return null;
+        }
     }
 }
